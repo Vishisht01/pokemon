0ef505b8d7684b10c18be4d4272678231e05bd86
Vishisht Rawat <Vishisht01@gmail.com>
2024-02-22 12:26:30 +0530

    Modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 'use client'
-import createApolloClient from "@/graphql/apolloClient";
-import { GET_ALL_POKEMON, GET_POKEMON, SEARCH_POKEMON } from "@/graphql/gqlQueries";
-import "./globals.css"
-import { Pokemon } from "@/types/pokemon";
 import Card from "@/components/Card";
-import { useRouter } from "next/router";
-import { ApolloProvider, LazyQueryHookExecOptions, OperationVariables, QueryResult, gql, useLazyQuery, useSuspenseQuery } from "@apollo/client";
-import Image from "next/image";
-import Icon from "../../public/pokemon_image.png"
+import { GET_ALL_POKEMON, GET_POKEMON, SEARCH_POKEMON } from "@/graphql/gqlQueries";
+import { Pokemon } from "@/types/pokemon";
+import { LazyQueryHookExecOptions, OperationVariables, QueryResult, useLazyQuery, useSuspenseQuery } from "@apollo/client";
+import { debounce } from 'lodash';
 import { ChangeEvent, useState } from "react";
-import { debounce } from 'lodash'
+import "./globals.css";
+import SearchCard from "@/components/SearchCard";
 
 
 export default function First() {
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [modal, setModal] = useState(false);
   const { data }: { data: { getAllPokemon: Pokemon[] } } = useSuspenseQuery(GET_ALL_POKEMON, { variables: { offset: 0, take: 20, reverse: true } });
   const [searchPokemon, { data: suggestions }]: [(options?: Partial<LazyQueryHookExecOptions<any, OperationVariables>> | undefined) => Promise<QueryResult<any, OperationVariables>>, { data: { getFuzzyPokemon: Pokemon[] } }] = useLazyQuery(SEARCH_POKEMON)
 
@@ -20,10 +19,10 @@ export default function First() {
 
   const handleSearch = async (e: ChangeEvent<HTMLInputElement>) => {
     try {
+      setShowSuggestions(true);
       await searchPokemon({ variables: { offset: 0, take: 10, reverse: true, pokemon: e.target.value } })
     } catch (error) {
       console.log(error);
-
     }
   }
 
@@ -32,94 +31,48 @@ export default function First() {
   const handlePokemonSelect = async (key: string) => {
     try {
       await getPokemon({ variables: { pokemon: key } });
+      setShowSuggestions(!showSuggestions);
+      setModal(true)
     } catch (error) {
       console.log(error);
-
     }
   }
   const handleSuggestionClick = (key: string) => {
     handlePokemonSelect(key);
   }
+  const handleModal = () => {
+    setModal(false)
+  }
+  console.log(showSuggestions);
 
   return (
     <>
-      <div className="container">
+      <div className="container" onClick={handleModal}>
         <div className="banner_container">
-          <div className={pokemon ? "content_search_section" : "content_section"}>
+          <div className="search_result">
+            {
+              modal && pokemon &&
+              <SearchCard pokemon={pokemon.getPokemon} />
+            }
+          </div>
+          <div className="content_section">
             <img src="pokemon_image.png" alt="hello" width={169} height={54} />
             <div className="banner_content">
-              {
-                pokemon ?
-                  (<>
-                    <div className="search_card">
-                      <div className="card_content">
-                        <div className="card_details">
-                          <div className="card_name">
-                            <h1 className="heading_font">{pokemon.getPokemon.species}</h1>
-                            <div className="card_subdetails">
-                              <div className="card_subheading">
-                                <span className="subHeading_font">Height</span>
-                                <span className="subHeading_font">Weight</span>
-                              </div>
-                              <div className="card_subheading">
-                                <span>
-                                  <p className="text_font">{pokemon.getPokemon.height}m</p>
-                                </span>
-                                <span>
-                                  <p className="text_font">{pokemon.getPokemon.weight}lbs</p>
-                                </span>
-                              </div>
-                            </div>
-                          </div>
-                          <div className="card_img_container">
-                            <img src={pokemon.getPokemon.shinyBackSprite} className="pokemon_img" alt={pokemon.getPokemon.key} />
-                          </div>
-                        </div>
-                        <span className="subHeading_font">Abilities</span>
-                        <div className="ability_container">
-                          <div className="ability_details">
-                            <h1 className="subHeading_font">First:</h1>
-                            <h1 className="subHeading_font">Hidden:</h1>
-                            <h1 className="subHeading_font">Second:</h1>
-                            <h1 className="subHeading_font">Special:</h1>
-                          </div>
-                          <div className="ability_details">
-                            <h1 className="subHeading_font">{pokemon.getPokemon?.abilities.first.name}</h1>
-                            <h1 className="subHeading_font">
-                              {pokemon.getPokemon.abilities?.hidden?.name ? pokemon.getPokemon.abilities?.hidden?.name : "No hidden ability"}
-                            </h1>
-                            <h1 className="subHeading_font">
-                              {pokemon.getPokemon.abilities?.second?.name ? pokemon.getPokemon.abilities?.second?.name : "No second ability"}
-                            </h1>
-                            <h1 className="subHeading_font">
-                              {pokemon.getPokemon.abilities?.special?.name ? pokemon.getPokemon.abilities?.special?.name : "No special ability"}
-                            </h1>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </>) :
-                  (
-                    <>
-                      <h1 className="heading_font">Pokemon</h1>
-                      <h4 className="text_font">Pokémon are creatures of various shapes and sizes that inhabit the fictional Pokémon world. Trainers, known as Pokémon trainers, catch and train these creatures to battle each other for sport. Each Pokémon species has its own unique abilities, characteristics, and evolutionary paths.</h4>
-                      <button className="banner_btn">Know More</button>
-                    </>
-                  )
-              }
+              <h1 className="heading_font">Pokemon</h1>
+              <h4 className="text_font">Pokémon are creatures of various shapes and sizes that inhabit the fictional Pokémon world. Trainers, known as Pokémon trainers, catch and train these creatures to battle each other for sport. Each Pokémon species has its own unique abilities, characteristics, and evolutionary paths.</h4>
+              <button className="banner_btn">Know More</button>
             </div>
           </div>
           <div className="image_section">
             <div className="search_container">
               <input placeholder="Search Pokemon" onChange={handleSearchDebounced} />
-              {suggestions && (
+              {showSuggestions && suggestions && (
                 <div className="suggestions_container">
                   {suggestions.getFuzzyPokemon.map((poke: Partial<Pokemon>) => (
                     <p className="suggestion" key={poke.key} onClick={() => handleSuggestionClick(poke.key)}>{poke.species}</p>
                   ))}
                 </div>
               )}
-
             </div>
             <div className="banner_image_container">
               <img src="banner_image.png" />
---
+++ src/components/SearchCard.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Pokemon } from "@/types/pokemon";
+
+interface SearchCardProps {
+    pokemon: Pokemon;
+}
+
+const SearchCard: React.FC<SearchCardProps> = ({ pokemon }) => {
+    return (
+        <div className="search_card">
+            <div className="card_content">
+                <div className="card_details">
+                    <div className="card_name">
+                        <h1 className="heading_font">{pokemon.species}</h1>
+                        <div className="card_subdetails">
+                            <div className="card_subheading">
+                                <span className="subHeading_font">Height</span>
+                                <span className="subHeading_font">Weight</span>
+                            </div>
+                            <div className="card_subheading">
+                                <span>
+                                    <p className="text_font">{pokemon.height}m</p>
+                                </span>
+                                <span>
+                                    <p className="text_font">{pokemon.weight}lbs</p>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card_img_container">
+                        <img src={pokemon.shinyBackSprite} className="pokemon_img" alt={pokemon.key} />
+                    </div>
+                </div>
+                <span className="subHeading_font">Abilities</span>
+                <div className="ability_container">
+                    <div className="ability_details">
+                        <h1 className="subHeading_font">First:</h1>
+                        <h1 className="subHeading_font">Hidden:</h1>
+                        <h1 className="subHeading_font">Second:</h1>
+                        <h1 className="subHeading_font">Special:</h1>
+                    </div>
+                    <div className="ability_details">
+                        <h1 className="subHeading_font">{pokemon.abilities?.first.name}</h1>
+                        <h1 className="subHeading_font">
+                            {pokemon.abilities?.hidden?.name ? pokemon.abilities?.hidden?.name : "No hidden ability"}
+                        </h1>
+                        <h1 className="subHeading_font">
+                            {pokemon.abilities?.second?.name ? pokemon.abilities?.second?.name : "No second ability"}
+                        </h1>
+                        <h1 className="subHeading_font">
+                            {pokemon.abilities?.special?.name ? pokemon.abilities?.special?.name : "No special ability"}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchCard;
