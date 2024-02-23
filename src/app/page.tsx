18e2496def20de3242b506011db3b10b099a7075
Vishisht Rawat <Vishisht01@gmail.com>
2024-02-23 12:09:01 +0530

    added banner data json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,14 @@ import { debounce } from 'lodash';
 import { ChangeEvent, useState } from "react";
 import "./globals.css";
 import SearchCard from "@/components/SearchCard";
-
+import banner from "../data/Banner.json"
 
 export default function First() {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [modal, setModal] = useState(false);
+
   const { data }: { data: { getAllPokemon: Pokemon[] } } = useSuspenseQuery(GET_ALL_POKEMON, { variables: { offset: 0, take: 20, reverse: false } });
+
   const [searchPokemon, { data: suggestions }]: [(options?: Partial<LazyQueryHookExecOptions<any, OperationVariables>> | undefined) => Promise<QueryResult<any, OperationVariables>>, { data: { getFuzzyPokemon: Pokemon[] } }] = useLazyQuery(SEARCH_POKEMON)
 
   const [getPokemon, { data: pokemon }]: [(options?: Partial<LazyQueryHookExecOptions<any, OperationVariables>> | undefined) => Promise<QueryResult<any, OperationVariables>>, { data: { getPokemon: Pokemon } }] = useLazyQuery(GET_POKEMON)
@@ -54,16 +56,16 @@ export default function First() {
             </div>
           }
           <div className="content_section">
-            <img src="images/pokemon_image.png" alt="hello" width={169} height={54} />
+            <img src={banner.content_section.image_src} alt={banner.content_section.image_alt} width={banner.content_section.image_width} height={banner.content_section.image_height} />
             <div className="banner_content">
-              <h1 className="heading_font">Pokemon</h1>
-              <h4 className="text_font">Pokémon are creatures of various shapes and sizes that inhabit the fictional Pokémon world. Trainers, known as Pokémon trainers, catch and train these creatures to battle each other for sport. Each Pokémon species has its own unique abilities, characteristics, and evolutionary paths.</h4>
-              <button className="banner_btn">Know More</button>
+              <h1 className="heading_font">{banner.content_section.heading}</h1>
+              <h4 className="text_font">{banner.content_section.text}</h4>
+              <button className="banner_btn">{banner.content_section.button_text}</button>
             </div>
           </div>
           <div className="image_section">
             <div className="search_container">
-              <input placeholder="Search Pokemon" onChange={handleSearchDebounced} />
+              <input placeholder={banner.image_section.search_placeholder} onChange={handleSearchDebounced} />
               {showSuggestions && suggestions && (
                 <div className="suggestions_container">
                   {suggestions.getFuzzyPokemon.map((poke: Partial<Pokemon>) => (
@@ -73,7 +75,7 @@ export default function First() {
               )}
             </div>
             <div className="banner_image_container">
-              <img src="images/banner_image.png" width={800} height={500} />
+              <img src={banner.image_section.banner_image_src} alt={banner.image_section.image_alt} width={banner.image_section.banner_image_width} height={banner.image_section.banner_image_height} />
             </div>
           </div>
         </div>
